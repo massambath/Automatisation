2eb1ac416f47af084c9c77b0ebd77659077c5390
massambath <160747267+massambath@users.noreply.github.com>
2025-11-27 23:04:11 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+URL_LOGIN = "https://example.com/login"
+USERNAME = "votre_email@example.com"
+PASSWORD = "votre_motdepasse"
+
+DOWNLOAD_PATH = "data/"  # où les factures PDF seront enregistrées
